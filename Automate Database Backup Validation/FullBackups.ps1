1a2ef059a093bbb87ab6292fb9365a548a3b3471
Joe Gavin <47458663+joegavin@users.noreply.github.com>
2023-02-27 13:42:56 -0800

    Add files via upload

---
+++ Automate Database Backup Validation/FullBackups.ps1
@@ -0,0 +1,3 @@
+Backup-DbaDatabase -SqlInstance WIN-K2NV3D17HHC\SERVER1 `
+-CompressBackup -CreateFolder -Path '\\WIN-K2NV3D17HHC\Backups\WIN-K2NV3D17HHC$SERVER1' `
+-Type Full 
---
+++ Automate Database Backup Validation/LogBackups.ps1
@@ -0,0 +1,6 @@
+Backup-DbaDatabase -SqlInstance WIN-K2NV3D17HHC\SERVER1 -Checksum `
+-CompressBackup -CreateFolder -Path \\WIN-K2NV3D17HHC\Backups\WIN-K2NV3D17HHC$SERVER1 `
+-Database AdventureWorks_1, AdventureWorks_2, AdventureWorks_3, AdventureWorks_4 -Type Log
+
+
+
