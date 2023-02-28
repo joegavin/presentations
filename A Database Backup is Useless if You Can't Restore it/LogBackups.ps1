b6378e667691d547277b2a707303463a3a5a56fb
Joe Gavin <47458663+joegavin@users.noreply.github.com>
2023-02-28 08:19:47 -0500

    Update LogBackups.ps1

--- A Database Backup is Useless if You Can't Restore it/LogBackups.ps1
+++ A Database Backup is Useless if You Can't Restore it/LogBackups.ps1
@@ -1,5 +1,5 @@
 Backup-DbaDatabase -SqlInstance WIN-K2NV3D17HHC\SERVER1 -Checksum `
--CompressBackup -CreateFolder -Path \\WIN-K2NV3D17HHC\Backups\WIN-K2NV3D17HHC$SERVER1 `
+-CompressBackup -CreateFolder -Path '\\WIN-K2NV3D17HHC\Backups\WIN-K2NV3D17HHC$SERVER1' `
 -Database AdventureWorks_1, AdventureWorks_2, AdventureWorks_3, AdventureWorks_4 -Type Log
 
 
