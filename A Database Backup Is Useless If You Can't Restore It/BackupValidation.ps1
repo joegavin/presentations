f4464ae8151ccb9160318ca96fcd5ab34fac452a
Joe Gavin <47458663+joegavin@users.noreply.github.com>
2023-08-30 14:06:48 -0400

    Update BackupValidation.ps1

--- A Database Backup Is Useless If You Can't Restore It/BackupValidation.ps1
+++ A Database Backup Is Useless If You Can't Restore It/BackupValidation.ps1
@@ -7,6 +7,7 @@ modules required: dbatools
 $Destination = "WIN-K2NV3D17HHC"
 $WorkDir = "C:\BackupValidation"
 $OutFile = "$WorkDir\BackupValidation.csv"
+$LogFile = "$WorkDir\Logfile.txt"
 $PSEmailServer = 'smtp.mymailserver.com'
 $SqlServers = Get-Content "$WorkDir\servers.txt" | Sort-Object # ordered list of sql server names in text file
 
@@ -16,15 +17,19 @@ Set-Location $WorkDir
 # delete old outfiles
 If (Test-Path "$OutFile"){Remove-Item "$OutFile"}
 
-# loop thru servers in $SqlServers array 
-Foreach ($SqlServer in $SqlServers) {
+# trap errors/warnings in log file
+Start-Transcript -Path $Logfile
+
+# Test-DbaLastBackup 
 Test-DbaLastBackup -SqlInstance $SqlServer -Destination $Destination | `
 Select-Object SourceServer,Database,FileExists,Size,RestoreResult,RestoreElapsed,DbccResult, `
               # https://www.millersystems.com/powershell-exporting-multi-valued-attributes-via-export-csv-cmdlet/
               @{Name='BackupFiles';Expression={[string]::join("`n", ($_.BackupFiles))}} | `
 Export-Csv -Path $OutFile -Append -NoTypeInformation
-}
 
-# email attachements
-$Attachments = @("$OutFile")
+# stop logging
+Stop-Transcript 
+
+# email attachemts
+$Attachments = @("$OutFile","$LogFile")
 Send-MailMessage -To "dba@mycompany.com" -From "testrestores@mycompany.com" -Subject "Test Restores Report" -Attachments $Attachments
